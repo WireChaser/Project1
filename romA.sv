945c72626ce14208f478f48715f1cf2b23c056fb
Jordi Marcial Cruz <jordi.marcialcruz@gmail.com>
2024-07-31 00:02:33 -0400

    Update romA.sv

--- romA.sv
+++ romA.sv
@@ -87,7 +87,7 @@ module romA
       altsyncram_component.clock_enable_output_a = "BYPASS",
       altsyncram_component.clock_enable_output_b = "BYPASS",
       altsyncram_component.indata_reg_b = "CLOCK0",
-      altsyncram_component.init_file = "matA_2.mif",					
+      altsyncram_component.init_file = "matA.mif",					
       altsyncram_component.intended_device_family = "Cyclone V",
       altsyncram_component.lpm_type = "altsyncram",
       altsyncram_component.numwords_a = 2**ADDR_WIDTH,
