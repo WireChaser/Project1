4285dd99582467ebfaaa30559a49402b5deb7939
Jordi Marcial Cruz <jordi.marcialcruz@gmail.com>
2024-07-30 23:22:23 -0400

    Update Controller.sv

--- Controller.sv
+++ Controller.sv
@@ -32,7 +32,7 @@ module Controller
    always_ff @(posedge clock) begin
       if (reset) begin
          current_state <= READ;
-     end else begin
+      end else begin
 	 case (current_state)
 	    READ:   current_state <= (last_addressB) ? LOAD : READ;   // Read data from ROM A and ROM B for multiplication
 	    LOAD:   current_state <= WRITE;                           // Load data from ROM C for final addition
@@ -54,42 +54,42 @@ module Controller
    end
 	
    /*** Clock Cycle Counter: Tracks the Total Cycles for the Matrix Operation ***/
-	always_ff @(posedge clock) begin
-		if (reset) begin
-			cycle_count <= 0;
-		end else if (~cycle_stop) begin        
-			cycle_count <= cycle_count + 24'd1; // Increment cycle count if not in END state
-		end 
-	end 
+   always_ff @(posedge clock) begin
+      if (reset) begin
+         cycle_count <= 0;
+      end else if (~cycle_stop) begin        
+	 cycle_count <= cycle_count + 24'd1; // Increment cycle count if not in END state
+      end 
+   end 
 	
-	/*** Address Counters for ROMs and RAM: Generates Addresses for Data Access ***/
-	always_ff @(posedge clock) begin
-		if (reset) begin
-			// Initialize all addresses to their starting values
-			romA_addrA   <= '0;         
-			romB_addrA   <= '0;        
-			romC_addrA   <= '0;
-			result_addrA <= '0;
-			romA_addrB   <= ROM_A_START_ADDR;   
-			romB_addrB   <= ROM_B_START_ADDR;   
-			romC_addrB   <= ROM_C_START_ADDR;  
-			result_addrB <= RAM_START_ADDR;   
-		end else if (current_state == READ) begin 
-			// Increment addresses for ROM A and B in the READ state
-			romA_addrA <= romA_addrA + 14'd4;    
-			romB_addrA <= romB_addrA + 7'd4;
-			romA_addrB <= romA_addrB + 14'd4;    
-			romB_addrB <= romB_addrB + 7'd4;     
-		end else if (current_state == LOAD) begin
-			// Increment addresses for ROM C in the LOAD state and resets ROM B to avoid overflow
-			romB_addrA <= '0;
-			romB_addrB <= '0;
-			romC_addrA <= romC_addrA + 7'd1;   
-			romC_addrB <= romC_addrB + 7'd1;   
-		end else if (current_state == CLEAR) begin 
-			// Increment addresses for result RAM in the CLEAR state
-			result_addrA <= result_addrA + 7'd1; 
-			result_addrB <= result_addrB + 7'd1;
-		end 
-	end
+   /*** Address Counters for ROMs and RAM: Generates Addresses for Data Access ***/
+   always_ff @(posedge clock) begin
+      if (reset) begin
+         // Initialize all addresses to their starting values
+	 romA_addrA   <= '0;         
+	 romB_addrA   <= '0;        
+	 romC_addrA   <= '0;
+	 result_addrA <= '0;
+	 romA_addrB   <= ROM_A_START_ADDR;   
+	 romB_addrB   <= ROM_B_START_ADDR;   
+	 romC_addrB   <= ROM_C_START_ADDR;  
+	 result_addrB <= RAM_START_ADDR;   
+   end else if (current_state == READ) begin 
+	 // Increment addresses for ROM A and B in the READ state
+	 romA_addrA <= romA_addrA + 14'd4;    
+	 romB_addrA <= romB_addrA + 7'd4;
+	 romA_addrB <= romA_addrB + 14'd4;    
+	 romB_addrB <= romB_addrB + 7'd4;     
+   end else if (current_state == LOAD) begin
+	 // Increment addresses for ROM C in the LOAD state and resets ROM B to avoid overflow
+	 romB_addrA <= '0;
+	 romB_addrB <= '0;
+	 romC_addrA <= romC_addrA + 7'd1;   
+	 romC_addrB <= romC_addrB + 7'd1;   
+   end else if (current_state == CLEAR) begin 
+	 // Increment addresses for result RAM in the CLEAR state
+	 result_addrA <= result_addrA + 7'd1; 
+	 result_addrB <= result_addrB + 7'd1;
+      end 
+   end
 endmodule : Controller
