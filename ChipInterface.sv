2449bf43a227a859630119ab823af15b30b16d6f
Jordi Marcial Cruz <jordi.marcialcruz@gmail.com>
2024-07-30 23:10:13 -0400

    Update ChipInterface.sv

--- ChipInterface.sv
+++ ChipInterface.sv
@@ -2,27 +2,26 @@
 
 module ChipInterface
   (input  logic       CLOCK_50,
-   input  logic [3:0] SW,
-   input  logic [0:0] KEY,
+   input  logic [1:0] SW,
    output logic [6:0] HEX0, HEX1, HEX2, HEX3, HEX4, HEX5);
 	
-	logic [23:0] sum, cycles;
-	logic [5:0] [3:0] result;
-	logic [5:0] [6:0] display;
+   logic [23:0] sum, cycles;
+   logic [5:0] [3:0] result;
+   logic [5:0] [6:0] display;
 	
-	assign {HEX5, HEX4, HEX3, HEX2, HEX1, HEX0} = display;
+   assign {HEX5, HEX4, HEX3, HEX2, HEX1, HEX0} = display;
 	
-	assign result = (SW[1]) ? sum : cycles;
+   assign result = (SW[1]) ? sum : cycles;
 	
-	MatrixMultiplier MatrixMultiplier
-	  (
-		 .CLOCK_50(CLOCK_50), 
-		 .reset(SW[0]),            
-		 .cycle_count(cycles),        			
-		 .ReadResultA(),    
-		 .ReadResultB(), 
-		 .ResultMatrixSum(sum) 
-	  );
+   MatrixMultiplier MatrixMultiplier
+      	(
+	 .CLOCK_50(CLOCK_50), 
+	 .reset(SW[0]),            
+	 .cycle_count(cycles),        			
+	 .ReadResultA(),    
+	 .ReadResultB(), 
+	 .ResultMatrixSum(sum) 
+	);
 	 
   genvar a;
   generate 
